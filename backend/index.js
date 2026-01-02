2cfb88366125a74aa52d38d3135504836bb2dd60
Artur <h4arthakobyan@gmail.com>
2026-01-02 23:09:28 +0100

    Add health check endpoints and moved all backend files from root folder into a backend folder

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res)=>{
-  res.send("Backend is running...");
+  res.json({ message: "API is working" });
 });
+
+app.get("/health", (req, res) =>{
+  res.json({status: "OK"})
+})
 
 app.listen(3000, ()=> {
   console.log("Server running on http://localhost:3000");
