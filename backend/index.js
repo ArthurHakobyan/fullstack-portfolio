5e9ee50344e79d4b1ef56cc2bc143988763028f9
Artur <h4arthakobyan@gmail.com>
2026-01-04 23:02:52 +0100

    Add middleware and POST /projects endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/",(req, res)=>{
   res.json({ message: "API is working" });
 });
@@ -15,3 +17,15 @@ app.listen(3000, ()=> {
 });
 
 // Test commit: checking GitHub token authentication
+
+app.post("/projects", (req,res)=>{
+  const {title, description} = req.body;
+
+  if(!title || !description) {
+    return res.status(400).json({error: "Title and description are required"})
+  }
+
+  return res.status(201).json({message: "Project created successfully",
+    project: {title, description}
+  })
+})
